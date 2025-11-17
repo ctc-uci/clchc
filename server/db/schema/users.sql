37a043b7f049027fa3be3cccc06eb5717a09e058
Ira Dharia <ira.dharia@gmail.com>
2025-11-17 15:41:58 -0800

    created user table schema

--- server/db/schema/users.sql
+++ server/db/schema/users.sql
@@ -1,20 +1,15 @@
--- Table: public.users
+-- ask about public.users
+DROP TABLE IF EXISTS public.users CASCADE;
+CREATE TYPE roles AS ENUM('master', 'ccm', 'ccs', 'viewer');
+CREATE TYPE statusType AS ENUM('approved', 'pending', 'rejected');
 
--- DROP TABLE IF EXISTS public.users;
-
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    email character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    firebase_uid character varying(128) COLLATE pg_catalog."default" NOT NULL,
-    role character varying(16) COLLATE pg_catalog."default" NOT NULL DEFAULT 'user'::character varying,
-    CONSTRAINT user_pkey PRIMARY KEY (id),
-    CONSTRAINT email UNIQUE (email),
-    CONSTRAINT firebase_uid UNIQUE (firebase_uid),
-    CONSTRAINT role_check CHECK (role::text = ANY (ARRAY['user'::character varying, 'admin'::character varying]::text[]))
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.users
-    OWNER to postgres;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firebase_uid VARCHAR(128) NOT NULL,
+  role roles NOT NULL,
+  first_name VARCHAR(16) NOT NULL,
+  last_name VARCHAR(16) NOT NULL,
+  email VARCHAR(32) NOT NULL,
+  status statusType  NOT NULL,
+  appt_calc_factor FLOAT,
+);
