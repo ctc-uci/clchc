a0531ebd9a2ab426a0b044d0d6b4927b029c7078
Nathan Ong <nathanong1217@gmail.com>
2025-11-19 22:15:55 -0800

    changed varchar to text and updated name to unique

--- server/db/schema/directory_categories.sql
+++ server/db/schema/directory_categories.sql
@@ -4,9 +4,9 @@ CREATE TYPE input_type AS ENUM('tag', 'text');
 
 CREATE TABLE directory_categories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     input_type input_type NOT NULL,
     is_required BOOLEAN NOT NULL,
     date_created TIMESTAMP DEFAULT NOW(),
-    column_order INT,
+    column_order INT
 );
--- server/db/schema/tags.sql
+++ server/db/schema/tags.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS tags CASCADE;
 CREATE TABLE tags (
   id SERIAL PRIMARY KEY,
   category_id INT NOT NULL,
-  tag_value VARCHAR(64) NOT NULL,
+  tag_value TEXT NOT NULL,
   CONSTRAINT category_id_fk FOREIGN KEY (category_id) 
   REFERENCES directory_categories(id) 
   ON DELETE CASCADE
