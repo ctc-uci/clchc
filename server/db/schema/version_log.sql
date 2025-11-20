f62d318064f0738a0cb41b0359196ca7febbdaff
Allison <allisonlyhuang@gmail.com>
2025-11-19 20:13:57 -0800

    fixing review

--- server/db/schema/version_log.sql
+++ server/db/schema/version_log.sql
@@ -4,12 +4,12 @@ CREATE TYPE action AS ENUM (
     'increment', 'decrement'
 );
 
-CREATE TABLE IF NOT exists version_log (
+CREATE TABLE IF NOT EXISTS version_log (
     id SERIAL PRIMARY KEY,
     user_id INT,
     quota_id INT,
     action action NOT NULL,
     timestamp TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (quota_id) REFERENCES quota(id) ON DELETE CASCADE,
+    FOREIGN KEY (quota_id) REFERENCES quota(id) ON DELETE CASCADE
 );
