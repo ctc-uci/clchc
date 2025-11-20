3d5eb74eccef9774f0d47e7f79eee6db90fbd0c3
Jennifer Yan <yj.jenniferyan@gmail.com>
2025-11-20 12:31:40 -0800

    fix quota table

--- server/db/schema/quota.sql
+++ server/db/schema/quota.sql
@@ -1,17 +1,15 @@
+CREATE TYPE appointment_type enum('inperson', 'telehealth'),
+
+DROP TABLE IF EXISTS quota;
 CREATE TABLE IF NOT EXISTS quota (
     id SERIAL PRIMARY KEY,
-    quota_id INT,
-    FOREIGN KEY (quota_id)
-        REFERENCES version_log(quota_id)
-        ON DELETE CASCADE,
-
     provider_id INTEGER NOT NULL,
-    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id)
+        REFERENCES location(id)
+        ON DELETE CASCADE
     quota INTEGER NOT NULL,
     progress INTEGER NOT NULL,
     hours INTEGER NOT NULL, 
     appointment_type appointment_type NOT NULL,
-    notes VARCHAR(256),
-    
-    CREATE TYPE appointment_type enum('inperson', 'telehealth'),
+    notes TEXT,
 );
