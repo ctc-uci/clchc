b910690da9d277eae84bf6113ba29de446219847
Nathan Nguyen <137689212+Nameless7631@users.noreply.github.com>
2025-11-20 17:38:05 -0800

    Update users.sql

--- server/db/schema/users.sql
+++ server/db/schema/users.sql
@@ -1,14 +1,15 @@
-DROP TABLE IF EXISTS public.users CASCADE;
-CREATE TYPE roles AS ENUM('master', 'ccm', 'ccs', 'viewer');
-CREATE TYPE statusType AS ENUM('approved', 'pending', 'rejected');
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TYPE role AS ENUM('master', 'ccm', 'ccs', 'viewer');
+CREATE TYPE status AS ENUM('approved', 'pending', 'rejected');
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   firebase_uid TEXT NOT NULL UNIQUE,
-  role roles NOT NULL,
+  role role NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   email TEXT NOT NULL UNIQUE,
-  status statusType  NOT NULL,
+  status status NOT NULL,
   appt_calc_factor FLOAT
 );
