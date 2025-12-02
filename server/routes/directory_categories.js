7ee4838600718d73a72ffca28a9b09d671329489
Meredith <pinkelephantloopsy@gmail.com>
2025-12-02 13:57:52 -0800

    Added CRUD routes

--- server/routes/directory_categories.js
+++ server/routes/directory_categories.js
@@ -9,11 +9,57 @@ export const usersRouter = Router();
 // Get all directory categories
 usersRouter.get("/:id", async (req, res) => {
   try {
-    const id = req.params;
+    const { id } = req.params;
     const categories = await db.query(`SELECT * FROM directory_categories WHERE id = $1`, [id]);
 
     res.status(200).json(keysToCamel(categories));
   } catch (err) {
     res.status(400).send(err.message);
   }
+});
+
+// Adds a new category
+usersRouter.post("/create", async (req, res) => {
+  try {
+    const { id, name, input_type, is_required, date_created, column_order } = req.body;
+    const categories = await db.query(
+      "INSERT INTO directory_categories (id, name, input_type, is_required, date_created, column_order) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [id, name, input_type, is_required, date_created, column_order]
+    );
+
+    res.status(200).json(keysToCamel(categories));
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+// Delete a category
+usersRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await admin.auth().deleteUser(id);
+    const categories = await db.query("DELETE FROM directory_categories WHERE id = $1", [
+      id,
+    ]);
+
+    res.status(200).json(keysToCamel(categories));
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Updates only the fields present in the request body
+usersRouter.patch("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, column_order } = req.body;
+        const categories = await db.query("UPDATE directory_categories SET name = $1 WHERE column_order = $2 RETURNING *",[
+            name, column_order
+        ]);
+        
+        res.status(200).json(keysToCamel(categories));
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
 });
