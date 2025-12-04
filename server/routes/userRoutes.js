588914faede1659eeca10e0efec79797b563f1e9
Rajat Kaliani <rkaliani@uci.edu>
2025-12-03 16:55:23 -0800

    fix: fixed functionality of post and put routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,11 +7,11 @@ const router = express.Router();
 // Create a new user
 router.post('/',async(req,res) =>{
     try {
-        const { email, firebaseUid } = req.body;
+        const { firebaseUid, role, firstName, lastName, email, status, apptCalcFactor } = req.body;
         
         const result = await db.query(
-        'INSERT INTO users (email, firebase_uid) VALUES ($1, $2) RETURNING *',
-        [email, firebaseUid]
+        'INSERT INTO users (firebase_uid, role, first_name, last_name, email, status, appt_calc_factor)VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+        [firebaseUid, role, firstName, lastName, email, status, apptCalcFactor]
         );
         
         res.status(201).json(keysToCamel(result));
@@ -34,6 +34,10 @@ router.get('/:id', async(req,res)=>{
         const {id} = req.params
 
         const result = await db.query('SELECT * from users WHERE id=$1',[id]);
+
+        if (!result || result.length === 0) {
+            return res.status(404).json({ error: "User not found." });
+        }
         res.status(200).json(keysToCamel(result));
     } catch(err){
         res.status(500).send(err.message);
@@ -43,13 +47,15 @@ router.get('/:id', async(req,res)=>{
 router.put('/:id', async(req,res)=>{
     try{
         const {id} = req.params;
-        const { email, firebaseUid } = req.body;
+        const {firebaseUid, role, firstName, lastName, email, status, apptCalcFactor} = req.body;
         
         const result = await db.query(
-        'UPDATE users SET email=$1, firebase_uid=$2 WHERE id=$3 RETURNING *',
-        [email, firebaseUid, id]
+        'UPDATE users SET firebase_uid=COALESCE($1, firebase_uid), role=COALESCE($2, role), first_name=COALESCE($3, first_name), last_name=COALESCE($4, last_name), email=COALESCE($5, email), status=COALESCE($6, status), appt_calc_factor=COALESCE($7, appt_calc_factor) WHERE id=$8 RETURNING *',
+        [firebaseUid, role, firstName, lastName, email, status, apptCalcFactor, id]
         );
-        
+        if (!result || result.length === 0) {
+            return res.status(404).json({ error: "User not found." });
+        }
         res.status(200).json(keysToCamel(result));
     } catch(err){
         res.status(500).send(err.message); 
@@ -61,6 +67,11 @@ router.delete('/:id', async(req,res)=>{
         const {id} = req.params;
 
         const result = await db.query('DELETE FROM users WHERE id=$1 RETURNING *',[id]);
+
+        if (!result || result.length === 0) {
+            return res.status(404).json({ error: "User not found." });
+        }
+
         res.status(200).json(keysToCamel(result));
     } catch(err){
         res.status(500).send(err.message);
