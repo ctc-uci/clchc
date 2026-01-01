88436910a8021aaa0f4e63e15c9915c88404da4f
Nathan Nguyen <nathnguyen7631@gmail.com>
2026-01-01 15:37:02 -0800

    adjustments to spacing and routes

--- server/routes/directory_categories.js
+++ server/routes/directory_categories.js
@@ -1,7 +1,5 @@
 import { keysToCamel } from "@/common/utils";
-import { admin } from "@/config/firebase";
 import { db } from "@/db/db-pgp"; // TODO: replace this db with
-import { verifyRole } from "@/middleware";
 import { Router } from "express";
 
 export const directoryCategoriesRouter = Router();
@@ -9,8 +7,8 @@ export const directoryCategoriesRouter = Router();
 // Get all directory categories
 directoryCategoriesRouter.get("/", async (req, res) => {
   try {
-    const { id } = req.params; 
-    const categories = await db.query(`SELECT * FROM directory_categories`, []);
+    const categories = await db.query(`SELECT * FROM directory_categories`);
+
     res.status(200).json(keysToCamel(categories));
   } catch (err) {
     res.status(400).send(err.message);
@@ -21,9 +19,11 @@ directoryCategoriesRouter.get("/", async (req, res) => {
 directoryCategoriesRouter.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
+
     const categories = await db.query(`SELECT * FROM directory_categories WHERE id = $1`, [id]);
-    if (categories.rows.length === 0) {
-      res.status(404).json({ error: "Not found" });
+
+    if (categories.length === 0) {
+      res.status(404).json({ error: "Category not found" });
     } else {
       res.status(200).json(keysToCamel(categories));
     }
@@ -35,11 +35,13 @@ directoryCategoriesRouter.get("/:id", async (req, res) => {
 // Adds a new category
 directoryCategoriesRouter.post("/", async (req, res) => {
   try {
-    const { name, inputType, isRequired, dateCreated, columnOrder } = req.body;
+    const { name, inputType, isRequired, columnOrder } = req.body;
+
     const categories = await db.query(
-      "INSERT INTO directory_categories (name, input_type, is_required, date_created, column_order) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-      [name, inputType, isRequired, dateCreated, columnOrder]
+      "INSERT INTO directory_categories (name, input_type, is_required, column_order) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, inputType, isRequired, columnOrder]
     );
+
     res.status(200).json(keysToCamel(categories));
   } catch (err) {
     res.status(500).send(err.message);
@@ -51,11 +53,13 @@ directoryCategoriesRouter.post("/", async (req, res) => {
 directoryCategoriesRouter.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const categories = await db.query("DELETE FROM directory_categories WHERE id = $1", [
-      id,
-    ]);
-    if (categories.rows.length === 0) {
-      res.status(404).json({ error: "Not found" });
+
+    const categories = await db.query("DELETE FROM directory_categories WHERE id = $1 RETURNING *", 
+      [id]
+    );
+
+    if (categories.length === 0) {
+      res.status(404).json({ error: "Category not found" });
     } else {
       res.status(200).json(keysToCamel(categories));
     }
@@ -65,29 +69,68 @@ directoryCategoriesRouter.delete("/:id", async (req, res) => {
 });
 
 // Updates only the fields present in the request body
-directoryCategoriesRouter.patch("/:id", async (req, res) => {
+directoryCategoriesRouter.put("/:id", async (req, res) => {
     try {
-        const { id } = req.params;
-        const fields = req.body;
-        if (Object.keys(req.body).length === 0) {
-            return res.status(200).send({ message: "No fields to update" });
+        const { id } = req.params
+        const {
+            name,
+            inputType,
+            isRequired,
+            dateCreated,
+            columnOrder
+        } = req.body;
+
+        const result = await db.query(
+            `UPDATE directory_categories
+            SET
+            name = COALESCE($1, name),
+            input_type = COALESCE($2, input_type),
+            is_required = COALESCE($3, is_required),
+            date_created = COALESCE($4, date_created),
+            column_order = COALESCE($5, column_order)
+            WHERE id = $6
+            RETURNING *`, 
+            [name, inputType, isRequired, dateCreated, columnOrder, id]
+        )
+        if (result.length === 0) {
+            return res.status(404).json({ error: `Category with id ${id} not found.`}); 
         }
-        const updates = [];
-        const values = [];
-        values.push(id);
-        let index = 2;
-        for (const key in fields) {
-          updates.push(`${key} = $${index}`);
-          values.push(fields[key]);
-          index++;
-        }
-        const categories = await db.query(`UPDATE directory_categories SET ${updates.join(", ")} WHERE id = $1 RETURNING *`, values);
-        if (categories.rows.length === 0) {
-          res.status(404).json({ error: "Not found" });
-        } else {
-          res.status(200).json(keysToCamel(categories));
+        else {
+            res.status(200).json(keysToCamel(result));
         }
     } catch (err) {
         res.status(400).send(err.message);
     }
 });
+
+// directoryCategoriesRouter.patch("/:id", async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const fields = req.body;
+
+//         if (Object.keys(req.body).length === 0) {
+//             return res.status(200).send({ message: "No fields to update" });
+//         }
+
+//         const updates = [];
+//         const values = [];
+//         values.push(id);
+//         let index = 2;
+
+//         for (const key in fields) {
+//           updates.push(`${key} = $${index}`);
+//           values.push(fields[key]);
+//           index++;
+//         }
+
+//         const categories = await db.query(`UPDATE directory_categories SET ${updates.join(", ")} WHERE id = $1 RETURNING *`, values);
+
+//         if (categories.length === 0) {
+//           res.status(404).json({ error: "Category not found" });
+//         } else {
+//           res.status(200).json(keysToCamel(categories));
+//         }
+//     } catch (err) {
+//         res.status(400).send(err.message);
+//     }
+// });
