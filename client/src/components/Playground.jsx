fd5145bccc2c42760526bd9b22987a4615dcdb4d
Meredith <pinkelephantloopsy@gmail.com>
2026-01-07 11:58:25 -0800

    Progress Bar Prototype

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -1,9 +1,15 @@
+import React, {useState} from "react";
 import { Box, Text } from "@chakra-ui/react";
+import ProgressBar from "./ProgressBar.jsx";
+
 
 export const Playground = () => {
+  const [ currentProgress, setCurrentProgress ] = useState(5);
+
   return (
     <Box>
       <Text>Place your items here for testing</Text>
+      <ProgressBar currentProgress={currentProgress} setCurrentProgress={setCurrentProgress}/>
     </Box>
   );
 };
