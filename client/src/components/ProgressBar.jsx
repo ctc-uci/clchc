1a5dfb8d064ec6562d7ba4f67325e50e8050324b
Meredith <pinkelephantloopsy@gmail.com>
2026-01-08 17:04:04 -0800

    Fixed buttons and styling

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,41 +1,56 @@
 import React from "react";
-import { Progress } from "@chakra-ui/react";
-import styles from "./ProgressBar.module.css";
+
+import { Button, Flex, Progress } from "@chakra-ui/react";
 
 export default function ProgressBar(props) {
-    const {
-        width = "10em",
-        currentProgress,
-        setCurrentProgress
-    } = props;
-    
+  const { width = "172px", currentProgress, setCurrentProgress } = props;
 
-    const handleDecrease = () => setCurrentProgress(prev => prev-1);
+  const handleDecrease = () => setCurrentProgress((prev) => prev - 1);
 
-    const handleIncrease = () => setCurrentProgress(prev => prev+1);
+  const handleIncrease = () => setCurrentProgress((prev) => prev + 1);
 
-    return(
-        <section
-            className={styles["progress-bar"]}
-        >
-            <button
-                className={styles.button}
-                onClick={handleDecrease}
-            >
-                {"\u2193"}
-            </button>
-            <Progress
-                value={currentProgress}
-                colorScheme="pink"
-                width={width}
-                
-            />
-            <button
-                className={styles.button}
-                onClick={handleIncrease}
-            >
-                {"\u2191"}
-            </button>
-        </section>
-    )
+  return (
+    <Flex
+      alignItems="center"
+      gap="5px"
+      padding={1}
+    >
+      <Button
+        onClick={handleDecrease}
+        width="17px"
+        minW={0}
+        px={0}
+        height="22px"
+        border="1px black solid"
+        background="white"
+        border-radius="5px"
+        fontSize="100%"
+      >
+        {"\u2193"}
+      </Button>
+      <Progress
+        value={currentProgress}
+        max="12"
+        colorScheme="gray"
+        width={width}
+        borderRadius={6}
+        border="1px lightgray solid"
+        background="gray.50"
+      />
+      <Button
+        onClick={handleIncrease}
+        width="17px"
+        minW={0}
+        px={0}
+        height="22px"
+        border="1px black solid"
+        background="black"
+        textColor="white"
+        _hover={{ background: "gray" }}
+        fontSize="100%"
+      >
+        {"\u2191"}
+      </Button>
+    </Flex>
+  );
 }
