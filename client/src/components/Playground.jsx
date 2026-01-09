4947c7314588717e3816a6c3ba198f4c3bb8ca79
Grace Yan <xgraceyan@gmail.com>
2026-01-09 13:51:49 -0800

    chore: Revert playground testing

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -1,12 +1,9 @@
 import { Box, Text } from "@chakra-ui/react";
 
-import QuotaDrawer from "./QuotaDrawer";
-
 export const Playground = () => {
   return (
     <Box>
       <Text>Place your items here for testing</Text>
-      <QuotaDrawer />
     </Box>
   );
 };
