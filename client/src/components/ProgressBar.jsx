516ecd1dfe13f870b5ab9b9105609740b9759078
Meredith <pinkelephantloopsy@gmail.com>
2026-01-10 12:54:16 -0800

    Skeleton for API (needs testing)

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect } from "react";
 import { Button, Flex, Progress, Icon, } from "@chakra-ui/react";
 import {ArrowUp, ArrowDown} from "lucide-react";
-import api from "./api.js";
+import { useBackendContext } from "../contexts/hooks/useBackendContext";
 
 export default function ProgressBar({currentProgress, setCurrentProgress, quotaID}) {
-  const handleDecrease = async (quotaID) => {
-    await api.put(quotaID, (prev - 1));
+  
+  // Since Playground.jsx is currently an unprotected route, there's no login so
+  // we can't use the auth context => the API connection does not pass
+  // I think we would have to put this component in a
+  // temporary protected route for testing purposes it?
+  // But for now, this seems like it might work.
+  const {backend} = useBackendContext();
+  const quotaRef = useRef();
+  //const [quota, setQuota] = useState();
+  useEffect(() => {
+    (async () => {
+      const { data } = await backend.get(`/quota/${quotaID}`);
+      console.log(data);
+      quotaRef.current = data;
+      setQuota(data);
+    })();
+  }, []);
+
+  const updateProgress = async (progress) => {
+    if (!quotaRef.current) return;
+    await backend.put(`/quota/${quotaID}`, {
+      ...quotaRef.current,
+      progress
+    });
+  }
+  
+  // Updating backend and frontend
+  const handleDecrease = async () => {
+    await updateProgress(currentProgress- 1);
     setCurrentProgress((prev) => prev - 1);
   }
 
-  const handleIncrease = async (quotaID) => {
-    await api.put(quotaID, (prev + 1));
+  const handleIncrease = async () => {
+    await updateProgress(currentProgress+ 1);
     setCurrentProgress((prev) => prev + 1);
-  };
-
+  }
+  
   return (
     <Flex
       alignItems="center"
