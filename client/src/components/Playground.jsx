2185173bb42bc5db82ecdae418a7d77ec2653f2d
Lindsay Lim <limlindsayj@gmail.com>
2026-01-14 23:07:51 -0800

    feat: drawer component created, missing functionality

--- client/src/components/Playground.jsx
+++ client/src/components/Playground.jsx
@@ -1,12 +1,7 @@
-import { Box, Text } from "@chakra-ui/react";
-
-
+import { ProviderDrawer } from "./ ProviderDrawer";
 export const Playground = () => {
-
   return (
-    <Box>
-      <Text>Place your items here for testing</Text>
-    </Box>
+    <ProviderDrawer/>
   );
 };
 
