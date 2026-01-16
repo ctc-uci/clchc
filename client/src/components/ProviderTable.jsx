f0b72a7ae71646546d2a0cefb77b6b148eaa416f
Meredith <pinkelephantloopsy@gmail.com>
2026-01-15 17:16:39 -0800

    Implemented provider table

--- client/src/components/ProviderTable.jsx
+++ client/src/components/ProviderTable.jsx
@@ -15,9 +15,12 @@ export default function ProviderTable({ providers, providerCategories }) {
    * Making table header by mapping categories from providerCategories
    * @returns <Thead> with each category as a cell in the header row.
    */
+  console.log(providers);
+  console.log(providerCategories);
+
   const Header = () => {
-    const columns = providerCategories.map((cat) => (
-      <Th key={cat.name}>{cat.name}</Th>
+    const columns = providerCategories.map((category) => (
+      <Th key={category.name}>{category.name}</Th>
     ));
 
     return (
@@ -27,40 +30,22 @@ export default function ProviderTable({ providers, providerCategories }) {
     );
   };
 
-  /**
-   * Based on each category, takes data from each provider to render
-   * the correct information in the respective cells.
-   *
-   * Will be used by Body subcomponent.
-   * @returns <Tr> with each cell of necessary provider info <Td>.
-   */
-  const ProviderRow = ({ provider }) => {
-    const getCells = () =>
-      providerCategories.map((cat) => (
-        <Td key={cat.name}>{provider.data[cat.name]}</Td>
-      ));
+  const Body = () => (
+    <Tbody>
+      {providers?.map((provider) => (
+        <Tr key={provider.id}>
 
-    return <Tr>{getCells()}</Tr>;
-  };
-
-  /**
-   * Uses ProviderRow subcomp to display provider data.
-   * @returns <Tbody> containing all rows of provider info.
-   */
-  const Body = () => {
-    const rows = providers.map((prov) => (
-      <ProviderRow
-        key={prov.id}
-        provider={prov}
-      />
-    ));
-    return <Tbody>{rows}</Tbody>;
-  };
+          {providerCategories.map((category) => (
+            <Td key={category.id}>{provider.data[category.name] ?? ""}</Td>
+          ))}
+        </Tr>
+      ))}
+    </Tbody>
+  );
 
   return (
     <TableContainer>
       <Table>
-        {/**Subcomps to simplify structure */}
         <Header />
         <Body />
       </Table>
