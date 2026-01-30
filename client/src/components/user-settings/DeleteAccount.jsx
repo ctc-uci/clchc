0d1dabfd95bb7dbf54b9e857bbd4d4c7fd93f994
Ira Dharia <ira.dharia@gmail.com>
2026-01-29 19:34:38 -0800

    wip delete account bug

--- client/src/components/user-settings/DeleteAccount.jsx
+++ client/src/components/user-settings/DeleteAccount.jsx
@@ -19,9 +19,17 @@ export default function DeleteAccount() {
   const { backend } = useBackendContext();
   const { currentUser } = useAuthContext();
   const [input, setInput] = useState("");
-  const handleDelete = () => {
-    backend.delete(`/users/${currentUser.uid}`);
+
+  const handleDelete = async () => {
+    try {
+      if (input === "DELETE") {
+        await backend.delete(`/users/${currentUser.uid}`);
+      }
+    } catch (e) {
+      alert("Incorrect.");
+    }
   };
+
   return (
     <Grid>
       <FormControl>
@@ -30,13 +38,22 @@ export default function DeleteAccount() {
           <Text>
             Are you sure? Deleting your account is permament and will remove all
             your information from the database. This action{" "}
-            <Text as={"span"} fontWeight={"bold"}>cannot</Text> be undone.
+            <Text
+              as={"span"}
+              fontWeight={"bold"}
+            >
+              cannot
+            </Text>{" "}
+            be undone.
           </Text>
           <Text>To confirm this, type "DELETE"</Text>
-          <Input value={input} />
+          <Input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
         </Grid>
+        <Button onClick={handleDelete}>Delete Account</Button>
       </FormControl>
-      <Button onClick={handleDelete}>Delete Account</Button>
     </Grid>
   );
 }
--- client/src/components/user-settings/PersonalInfo.jsx
+++ client/src/components/user-settings/PersonalInfo.jsx
@@ -1,79 +1,87 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect, useState } from "react";
+
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    Input,
-    Flex,
-    Grid,
-    Button,
-    Text
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Grid,
+  Input,
+  Text,
 } from "@chakra-ui/react";
+
 import useUser from "./useUser";
 
-export default function PersonalInfo(){
-    const { userInfo, setUserInfo, errorMessage, updateUser } = useUser();
+export default function PersonalInfo() {
+  const { userInfo, setUserInfo, errorMessage, updateUser } = useUser();
 
-  
-    if(!userInfo){return null};
-    /**
-     * useEffect(function, dependencyArray)
-     * useEffect(() => {
-     * }, [])    [] means no dependencies which means useEffect only runs the first time you render/mount
-     * If you do have dependencies, it would run the effect every time the dependency changes
-     */
+  if (!userInfo) {
+    return null;
+  }
+  /**
+   * useEffect(function, dependencyArray)
+   * useEffect(() => {
+   * }, [])    [] means no dependencies which means useEffect only runs the first time you render/mount
+   * If you do have dependencies, it would run the effect every time the dependency changes
+   */
 
-    const updateUserProp = (key, value) => {
-        setUserInfo(prev => ({...prev, [key]:value}))
-    }
+  const updateUserProp = (key, value) => {
+    setUserInfo((prev) => ({ ...prev, [key]: value }));
+  };
 
+  /**
+   * const funcName = async () => {
+   *     const {content} = await promise();
+   * }
+   */
 
-    /**
-     * const funcName = async () => {
-     *     const {content} = await promise();
-     * }
-     */
-    
-    return(
+  return (
+    <Grid
+      align="stretch"
+      spacing="2em"
+      backgroundColor="#ddd"
+      borderRadius="1em"
+      padding="1.5em"
+    >
+      <FormControl>
         <Grid>
-
-            <FormControl>
-                <Grid>
-                    <FormLabel>First name</FormLabel>
-                    <Input
-                        value={userInfo.firstName}
-                        onChange={(e) => updateUserProp("firstName", e.target.value)}
-                    />
-                </Grid>
-            </FormControl>
-            <FormControl>
-                <Grid>
-                    <FormLabel>Last name</FormLabel>
-                    <Input
-                        value={userInfo.lastName}
-                        onChange={(e) => updateUserProp("lastName", e.target.value)}
-                    />
-                </Grid>
-            </FormControl><FormControl>
-                <Grid>
-                    <FormLabel>Email address</FormLabel>
-                    <Input
-                        value={userInfo.email}
-                        onChange={(e) => updateUserProp("email", e.target.value)}
-                    />
-                </Grid>
-            </FormControl>
-            <FormControl>
-                <Grid>
-                    <FormLabel>Role</FormLabel>
-                    <Input
-                        value={userInfo.role}
-                        readOnly={true}
-                    />
-                </Grid>
-            </FormControl>
-            <Button onClick={updateUser}>Apply changes</Button>
-            <Text>{errorMessage}</Text>
+          <FormLabel>First name</FormLabel>
+          <Input
+            value={userInfo.firstName}
+            onChange={(e) => updateUserProp("firstName", e.target.value)}
+          />
         </Grid>
-    )
+      </FormControl>
+      <FormControl>
+        <Grid>
+          <FormLabel>Last name</FormLabel>
+          <Input
+            value={userInfo.lastName}
+            onChange={(e) => updateUserProp("lastName", e.target.value)}
+          />
+        </Grid>
+      </FormControl>
+      <FormControl>
+        <Grid>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            value={userInfo.email}
+            onChange={(e) => updateUserProp("email", e.target.value)}
+          />
+        </Grid>
+      </FormControl>
+      <FormControl>
+        <Grid>
+          <FormLabel>Role</FormLabel>
+          <Input
+            value={userInfo.role}
+            readOnly={true}
+          />
+        </Grid>
+      </FormControl>
+      <Button onClick={updateUser}>Apply changes</Button>
+      <Text>{errorMessage}</Text>
+    </Grid>
+  );
 }
