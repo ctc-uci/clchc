61d4a9fbd9161c98961ddc812dc226905d46a537
Ira Dharia <ira.dharia@gmail.com>
2026-02-01 17:21:10 -0800

    removed extra imports in logout

--- client/src/components/user-settings/LogoutModal.jsx
+++ client/src/components/user-settings/LogoutModal.jsx
@@ -1,56 +1,57 @@
 import React, { useEffect, useState } from "react";
 
 import {
-    Button,
-    HStack,
-    Grid,
-    Text,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    ModalHeader,
-    ModalOverlay
+  Button,
+  Grid,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
 } from "@chakra-ui/react";
 
-
 import { useRoleContext } from "@/contexts/hooks/useRoleContext";
-import {
-    Route,
-    BrowserRouter as Router,
-    Routes,
-    useNavigate,
-} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { Login } from "/src/components/login/Login.tsx";
 
 export default function LogoutModal({ isOpen, onClose }) {
-    const { role } = useRoleContext();
-    const [userInfo] = useState();
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        navigate("/login");
-    };
+  const { role } = useRoleContext();
+  const [userInfo] = useState();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/login");
+  };
 
-    return (
-        <Modal
-            isOpen={isOpen}
-            onClose={onClose}
-        >
-            <ModalOverlay />
-            <ModalContent>
-                <Grid placeItems="center">
-                    <ModalHeader fontSize="3xl">Logout</ModalHeader>
-                    <ModalBody fontSize="1.25em">Are you sure you want to log out of {role}?</ModalBody>
-                    <ModalFooter>
-                        <HStack gap="1.5em">
-                            <Button onClick={onClose}>Cancel</Button>
-                            <Button onClick={handleLogout} backgroundColor="#bbb">Logout</Button>
-                        </HStack>
-                    </ModalFooter>
-                </Grid>
-            </ModalContent>
-        </Modal >
-    );
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <Grid placeItems="center">
+          <ModalHeader fontSize="3xl">Logout</ModalHeader>
+          <ModalBody fontSize="1.25em">
+            Are you sure you want to log out of {role}?
+          </ModalBody>
+          <ModalFooter>
+            <HStack gap="1.5em">
+              <Button onClick={onClose}>Cancel</Button>
+              <Button
+                onClick={handleLogout}
+                backgroundColor="#bbb"
+              >
+                Logout
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </Grid>
+      </ModalContent>
+    </Modal>
+  );
 }
