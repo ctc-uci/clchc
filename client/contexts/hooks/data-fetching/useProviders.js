95a2441f699400d22ba60e329a1aac835f4f60b0
Jennifer <yj.jenniferyan@gmail.com>
2026-02-02 20:27:06 -0800

    fix: import useQueryClient

--- client/contexts/hooks/data-fetching/useProviders.js
+++ client/contexts/hooks/data-fetching/useProviders.js
@@ -1,15 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { api } from '../../../../server/api.js'
 
 export function useProviders() {
   return useQuery({
     queryKey: ['providers'],
     queryFn: api.providers.getAll(),
+    staleTime: 60 * 1000, 
     refetchInterval: 60 * 1000, // 1 min
   });
 }
 
 export const useCreateProvider = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (newProvider) => api.quotas.create(newProvider),
     onSuccess: () => {
