85d1b801283009f37301423724f80d066527e80a
Meredith <pinkelephantloopsy@gmail.com>
2026-02-11 15:51:05 -0800

    Implemented gradient blur

--- client/src/components/layout/layout.jsx
+++ client/src/components/layout/layout.jsx
@@ -1,13 +1,14 @@
+import { Box } from "@chakra-ui/react"
+
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Layout = () => {
     return (
-        <>
+        <Box minH="100vh" paddingBottom="calc(5vh + 80px)">
             <Navbar/>
             <Outlet/>
-        </>
-
+        </Box>
     );
 }
 
