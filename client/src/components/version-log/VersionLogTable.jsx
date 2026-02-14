9e50ea25fa52dea0a74149dc33230e9ede3068bd
ShyelSingh <ShyelSingh321@gmail.com>
2026-02-13 16:46:11 -0800

    added plus sign for quota increments rather than pure number

--- client/src/components/version-log/VersionLogTable.jsx
+++ client/src/components/version-log/VersionLogTable.jsx
@@ -92,7 +92,7 @@ const VersionLogTable = ({ loading, logs }) => {
                           as="span"
                           color="green.500"
                         >
-                          {entry.delta}
+                          +{entry.delta}
                         </Text>
                       </>
                     )}
