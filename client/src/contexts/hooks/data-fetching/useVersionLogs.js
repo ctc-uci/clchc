ea7c7a596cc58b3f60382c8861cd1733e6c94a0d
Jennifer <yj.jenniferyan@gmail.com>
2026-02-17 21:46:24 -0800

    chore: migrate version log updating to react-query caching

--- client/src/contexts/hooks/data-fetching/useVersionLogs.js
+++ client/src/contexts/hooks/data-fetching/useVersionLogs.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useApi } from '@/api.js'
 
 export const useVersionLogs = ({ q } = {}) => {
@@ -17,4 +17,19 @@ export const useVersionLogs = ({ q } = {}) => {
         staleTime: 60 * 1000,
         refetchInterval: 60 * 1000,
     });
+}
+
+export const useCreateLog = () => {
+  const { versionLogs } = useApi();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data) => {
+      console.log("Creating log (react-query)", data);
+      return versionLogs.create(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['logs'], { exact: false });
+    },
+  })
 }
