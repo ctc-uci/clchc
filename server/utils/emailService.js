c66517c19a9b0ad3056d8bb0fd821c1db72e1d3a
Meredith <pinkelephantloopsy@gmail.com>
2026-02-20 15:12:36 -0800

    Implemented functionality for notifying approved user.

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -54,3 +54,47 @@ export async function notifyCcmNewUserRequest(name, email) {
     console.error("Error sending mail", err);
   }
 }
+
+
+export async function notifyApprovedNewUser(name, email) {
+  // Validate email/password, early return if not configured.
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_APP_PASSWORD) {
+    console.warn("Email service not configured.");
+    return;
+  }
+
+  // Sanitize
+  const sanitizedName = escapeHtml(name);
+  const sanitizedEmail = escapeHtml(email);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_APP_PASSWORD,
+    },
+  });
+  try {
+    const emailMessage = `
+      <div style="font-family: Arial, sans-serif;">
+        <h1>User Approved</h1>
+        <p>Account has been approved for</p>
+        <p><strong>Name:</strong> ${sanitizedName}</p>
+        <p><strong>Email:</strong> ${sanitizedEmail}</p>
+        <p>Welcome!</a></p>
+      </div>
+    `;
+
+    await transporter.sendMail({
+      from: `"Admin" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "User Approval Notice",
+      text: `Your account has been approved.`,
+      html: emailMessage,
+    });
+
+    console.log("New user email sent");
+  } catch (err) {
+    console.error("Error sending mail", err);
+  }
+}
