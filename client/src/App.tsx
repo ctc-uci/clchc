85937a4f860e26fd84aadc53b6ca2a90cb52e279
ShyelSingh <ShyelSingh321@gmail.com>
2026-04-05 22:20:11 -0700

    fixed cubic issues of exit trigger for fade in animation. sped up animation slightly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,7 +36,8 @@ const AppRoutes = () => {
       key={location.pathname}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.15 }}
     >
       <Routes location={location}>
                 <Route element={<Layout/>}>
