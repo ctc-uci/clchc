6b4cf1b2e503af89b52d637326339c3b44e9d5aa
Meredith <pinkelephantloopsy@gmail.com>
2026-04-13 23:40:32 -0700

    Changed let to const

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -5,7 +5,7 @@ let task;
 let running = false;
 
 // temp: 2AM every day to keep only a 7-day log
-let interval = "0 2 * * *"
+const interval = "0 2 * * *"
 
 const startCron = () => {
     console.log("cron started");
