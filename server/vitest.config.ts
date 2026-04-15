88571631bd025e9e378ead9e53b5e457c84e11a7
Elliott Satchel Escalante <48373238+Satchel05@users.noreply.github.com>
2026-04-15 01:31:05 -0700

    Implement formal testing suite for deleting stale logs job

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,11 +1,14 @@
+// packages/api/vitest.config.ts
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [tsconfigPaths()],
   test: {
-    environment: "node",
-    include: ["src/__tests__/**/*.test.{ts,js}"],
-    watch: false,
+    env: loadEnv("test", process.cwd(), ""),
+    pool: "forks", // safer for DB connections
+    poolOptions: {
+      forks: { singleFork: true },
+    },
   },
-});
+}));
