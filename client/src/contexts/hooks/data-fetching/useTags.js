9bba358d67c0c03878490eae60e114259c92ef3e
Jennifer <yj.jenniferyan@gmail.com>
2026-04-29 20:41:39 -0700

    feaT: update use tags hook to support adding and deleting

--- client/src/contexts/hooks/data-fetching/useTags.js
+++ client/src/contexts/hooks/data-fetching/useTags.js
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { useApi } from "@/api.js";
 
 export function useTags() {
@@ -15,5 +15,31 @@ export function useTags() {
     staleTime: 60 * 1000,
     refetchInterval: 60 * 1000, // 1 min
     suspense: false,
+  });
+}
+
+export function useCreateTag() {
+  const queryClient = useQueryClient();
+  const { tags } = useApi();
+
+  return useMutation({
+    mutationFn: (newTag) => tags.create(newTag),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tags"] });
+      queryClient.invalidateQueries({ queryKey: ["tag"], exact: false });
+    },
+  });
+}
+
+export function useDeleteTag() {
+  const queryClient = useQueryClient();
+  const { tags } = useApi();
+
+  return useMutation({
+    mutationFn: ({ id }) => tags.delete(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tags"] });
+      queryClient.invalidateQueries({ queryKey: ["tag"], exact: false });
+    },
   });
 }
